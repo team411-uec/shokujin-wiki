e35ee34121244fcf0e5c9823693e3247950a811c
Keita Ito <eguchi@eguchi.cc>
2025-02-01 02:42:23 +0900

    feat: prismaとauthjsの接続

--- website/src/auth.ts
+++ website/src/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google, GitHub],
 });

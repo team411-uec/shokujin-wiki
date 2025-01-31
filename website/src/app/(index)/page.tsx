cc84e1969e0c4889ebce8916a965f319cedfade6
Keita Ito <eguchi@eguchi.cc>
2025-01-31 22:08:27 +0900

    chore: ボタンの確認

--- website/src/app/(index)/page.tsx
+++ website/src/app/(index)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function IndexPage() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button variant="default">Click me!</Button>
+    </div>
+  );
 }

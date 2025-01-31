31e7f9d1010bbbbfc6fd3fcdefe947e3f3a259e5
Keita Ito <eguchi@eguchi.cc>
2025-01-31 18:41:14 +0900

    chore: PrettierとESLintの構成

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
+// @ts-check
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +14,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+  eslintConfigPrettier,
+);

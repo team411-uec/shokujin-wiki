e9351a6ec6add4a05af654dca3d8f82df2bed30d
Keita Ito <eguchi@eguchi.cc>
2025-01-31 22:20:08 +0900

    feat: ダークモード対応

--- website/src/app/(index)/page.tsx
+++ website/src/app/(index)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./_components/mode-toggle";
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button variant="default">Click me!</Button>
+      <ModeToggle />
     </div>
   );
 }
--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,11 +1,21 @@
 import { PropsWithChildren } from "react";
 
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="ja" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

3bf32bc50eafc4cd9115dc545d09a970da3e5c48
Keita Ito <eguchi@eguchi.cc>
2025-02-01 14:43:27 +0900

    feat: サインインページにリダイレクト機能を追加

--- website/src/app/signin/page.tsx
+++ website/src/app/signin/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@/auth";
 import { SigninForm } from "./_components/signin-form";
+import { redirect } from "next/navigation";
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center">
       <div className="w-full max-w-md">
--- website/src/components/ui/sidebar.tsx
+++ website/src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
   Tooltip,
@@ -203,6 +203,7 @@ const Sidebar = React.forwardRef<
     if (isMobile) {
       return (
         <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+          <SheetTitle className="sr-only"></SheetTitle>
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"

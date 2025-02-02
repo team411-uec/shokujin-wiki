43168322428a0bdcc5a88421bd7b3fb0ec283133
Keita Ito <eguchi@eguchi.cc>
2025-02-03 00:41:04 +0900

    feat: サインインページにユーザー情報の取り扱いに関する注意事項を追加

--- website/src/app/%5Fsignin/page.tsx
+++ website/src/app/%5Fsignin/page.tsx
@@ -3,6 +3,7 @@ import { SigninForm } from "./_components/signin-form";
 import { redirect } from "next/navigation";
 import { AppHeader } from "@/components/app-header";
 import { Metadata } from "next";
+import { Viewer } from "@/components/viewer";
 
 export const metadata: Metadata = {
   title: "ログイン",
@@ -24,6 +25,21 @@ export default async function SigninPage() {
       <div className="flex items-center justify-center">
         <div className="max-w-md pt-16">
           <SigninForm />
+          <Viewer>
+            <small>
+              <ol>
+                <li>
+                  本サービスは、ユーザーの個人情報を第三者に提供することはありません。
+                </li>
+                <li>
+                  連携先から得られるメールアドレスを含む個人情報については、ユーザーの識別にのみ使用します。ログインすることによりこれらに同意することとします。
+                </li>
+                <li>
+                  LINEログインに失敗する場合、メールアドレスの登録をした上でログイン時にメールアドレスの共有を有効にしてください。
+                </li>
+              </ol>
+            </small>
+          </Viewer>
         </div>
       </div>
     </>

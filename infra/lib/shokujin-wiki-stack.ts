6a6b0a7eecd135b74f303bebab44bcc7498321dc
Keita Ito <eguchi@eguchi.cc>
2025-02-02 14:53:46 +0900

    chore: PrettierとESLintの構成

--- infra/lib/shokujin-wiki-stack.ts
+++ infra/lib/shokujin-wiki-stack.ts
@@ -1,16 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class ShokujinWikiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ShokujinWikiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, "ResourceBucket", {});
   }
 }

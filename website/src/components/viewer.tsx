6176397056d1f98b2e9f1a05846cf4a54603c2af
Keita Ito <eguchi@eguchi.cc>
2025-02-02 22:12:36 +0900

    chore: ダークモードを改善 (#6)

--- website/src/components/viewer.tsx
+++ website/src/components/viewer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Viewer({ children }: PropsWithChildren) {
   return (
-    <div className="prose prose-slate max-w-none dark:prose-invert prose-indigo">
+    <div className="prose prose-slate max-w-none dark:prose-invert prose-indigo dark:prose-blue">
       {children}
     </div>
   );

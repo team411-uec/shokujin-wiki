0c9d3b88cd6b726f9365e143de8236c7c67b618b
Keita Ito <eguchi@eguchi.cc>
2025-02-02 15:29:20 +0900

    feat: s3の構成

--- infra/README.md
+++ infra/README.md
@@ -1,14 +1,9 @@
-# Welcome to your CDK TypeScript project
+# shokujin-wiki (infra)
 
-This is a blank project for CDK development with TypeScript.
+```bash
+cp .env.example .env
+```
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-- `npm run build` compile typescript to js
-- `npm run watch` watch for changes and compile
-- `npm run test` perform the jest unit tests
-- `npx cdk deploy` deploy this stack to your default AWS account/region
-- `npx cdk diff` compare deployed stack with current state
-- `npx cdk synth` emits the synthesized CloudFormation template
+```bash
+npm install
+```
--- infra/lib/shokujin-wiki-stack.ts
+++ infra/lib/shokujin-wiki-stack.ts
@@ -1,11 +1,39 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+
+interface ShokujinWikiStackProps extends cdk.StackProps {
+  certificateArn: string;
+}
 
 export class ShokujinWikiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ShokujinWikiStackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, "ResourceBucket", {});
+    const certificate = certificatemanager.Certificate.fromCertificateArn(
+      this,
+      "ResourceCertificate",
+      props.certificateArn,
+    );
+
+    const bucket = new s3.Bucket(this, "ResourceBucket", {
+      bucketName: `shokujin-wiki-resources-${this.account}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin:
+          cloudfront_origins.S3BucketOrigin.withOriginAccessControl(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      certificate: certificate,
+      domainNames: ["s3.shokujin.com"],
+    });
   }
 }

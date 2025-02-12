3a0e255f2cd8f3c80beabadd05e4ac5ebe0e1ae5
Keita Ito <keitaito@keitaito.net>
2025-02-12 23:10:28 +0900

    feat: カテゴリーを実装 (#23)

--- website/src/components/article-viewer.tsx
+++ website/src/components/article-viewer.tsx
@@ -2,10 +2,13 @@ import { prisma } from "@/prisma";
 import { MarkdownViewer } from "./markdown-viewer";
 import { Viewer } from "./viewer";
 import { format } from "date-fns";
+import { Category } from "@prisma/client";
+import { Badge } from "./ui/badge";
 
 interface ArticleViewerProps {
   slug: string;
   content: string;
+  category: Category | null;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -13,6 +16,7 @@ interface ArticleViewerProps {
 export async function ArticleViewer({
   slug,
   content,
+  category,
   createdAt,
   updatedAt,
 }: ArticleViewerProps) {
@@ -27,14 +31,19 @@ export async function ArticleViewer({
   return (
     <Viewer>
       <h1 className="mb-0">{decodeURIComponent(slug)}</h1>
-      <p className="my-0 text-right">
-        <small>
+      <div className="my-0 flex items-start">
+        {category && (
+          <Badge variant="outline" className="mt-2">
+            {category.name}
+          </Badge>
+        )}
+        <small className="ml-auto">
           作成: {format(createdAt, "yyyy年M月d日")}{" "}
           {updatedAt !== createdAt && (
             <>最終更新: {format(updatedAt, "yyyy年M月d日")}</>
           )}
         </small>
-      </p>
+      </div>
       <MarkdownViewer content={content} existSlugs={existSlugs} />
     </Viewer>
   );
